b54b7c519e3311f8aa3efe747a24e893f8c2b16e
mahadi24t <mahadi24t@outlook.com>
2025-04-06 14:38:28 +0600

    variables

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ let inputBtn =  document.getElementById('input-btn');
 inputBtn.addEventListener('click', function(){
    console.log("Button clicked!");
 });
+
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+let myLeads = [];
+let inputEl = document.getElementById('input-el');
