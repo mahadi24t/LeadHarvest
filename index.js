7d0e07739a086d7b021766686c194d18db3e3d63
mahadi24t <mahadi24t@outlook.com>
2025-04-06 10:04:04 +0600

    addEventListener

---
+++ index.js
@@ -0,0 +1,16 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+//savIn = document.getElementById('input-btn');
+
+// console.log(savIn);
+
+// function savInClick(){
+//    console.log("HButton clicked!");
+// }
+
+//savInClick();
+
+let inputBtn =  document.getElementById('input-btn');
+inputBtn.addEventListener('click', function(){
+   console.log("Button clicked!");
+});
