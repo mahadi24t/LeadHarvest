d6610e4d41d3f17842d6764f3e162ba442e03c5c
mahadi24t <mahadi24t@outlook.com>
2025-04-06 15:19:13 +0600

    Push the value from the input field

--- index.js
+++ index.js
@@ -12,13 +12,16 @@
 
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
 let myLeads = [];
+let inputEl = document.getElementById('input-el');
 
 
 let inputBtn =  document.getElementById('input-btn');
+
+
 inputBtn.addEventListener('click', function(){
    // console.log("Button clicked!");
    // Push the value "www.awesomelead.com" to myArray when the input button is clicked
-   myLeads.push("www.awesomelead.com");
+   myLeads.push(inputEl.value);
    console.log(myLeads);
 });
 
@@ -27,4 +30,3 @@ inputBtn.addEventListener('click', function(){
 // inputEl -> should be assigned to the text input field
 
 
-let inputEl = document.getElementById('input-el');
