3333f501335e0eaca55a845e4d33eeb33e309013
mahadi24t <mahadi24t@outlook.com>
2025-04-09 23:56:43 +0600

    lazyday

--- index.js
+++ index.js
@@ -10,34 +10,43 @@
 
 //savInClick();
 
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
-let myLeads = [];
-let inputEl = document.getElementById('input-el');
+// // Push the value "www.awesomelead.com" to myArray when the input button is clicked
+
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+// let inputEl = document.getElementById('input-el');
 
 
-let inputBtn =  document.getElementById('input-btn');
+// let inputBtn =  document.getElementById('input-btn');
 
 
-inputBtn.addEventListener('click', function(){
-   // console.log("Button clicked!");
-   // Push the value "www.awesomelead.com" to myArray when the input button is clicked
-   myLeads.push(inputEl.value);
-   console.log(myLeads);
-});
+// inputBtn.addEventListener('click', function(){
+//    // console.log("Button clicked!");
+//    // Push the value "www.awesomelead.com" to myArray when the input button is clicked
+//    myLeads.push(inputEl.value);
+//    console.log(myLeads);
+// });
 
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    // Push the value from the inputEl into the myLeads array 
-    // instead of the hard-coded "www.awesomeleads.com" value
-    // Google -> "get value from input field javascript"
-    myLeads.push("www.awesomelead.com")
-    console.log(myLeads)
+   myLeads.push(inputEl.value)
+   console.log(myLeads)
+   
 })
+
+
+for (let i = 0; i < myLeads.length; i++) {
+   // // console.log(myLeads[i])
+   // ulEl.innerHTML +=  "<li>" + myLeads[i] + "</li>"
+   const li = document.createElement("li");
+   li.textContent = myLeads[i];
+   ulEl.appendChild(li);
+}
 
 
