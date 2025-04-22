d20cb0601cba68e4e46991bad133a8a4d180978d
mahadi24t <mahadi24t@outlook.com>
2025-04-22 23:59:05 +0600

    renderfunction

--- index.js
+++ index.js
@@ -29,27 +29,24 @@
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-   myLeads.push(inputEl.value)
-   console.log(myLeads)
-   
+    myLeads.push(inputEl.value)
+    // 2. Call the renderLeads() function
+    renderLeads()
 })
 
-let listItems = "";
-
-for (let i = 0; i < myLeads.length; i++) {
-   // // console.log(myLeads[i])
-   listItems +=  "<li>" + myLeads[i] + "</li>"
-   // const li = document.createElement("li");
-   // li.textContent = myLeads[i];
-   // ulEl.appendChild(li);
+// 1. Wrap the code below in a renderLeads() function
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems  
 }
-
-ulEl.innerHTML=  listItems
 
 
